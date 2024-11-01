d34ddaa4e03ace8ea4744443fe698b4c93d8ebe5
Ordonez96 <vasquez9600@gmail.com>
2024-11-01 16:07:07 -0600

    as update img

--- prjoim/appoim/models.py
+++ prjoim/appoim/models.py
@@ -5,6 +5,7 @@ class Proyecto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     descripcion_corta = models.TextField( null=True, blank=True)
+    img = models.ImageField(upload_to='proyectos/', null=True, blank=True)
 
     def __str__(self):
         return self.nombre
--- prjoim/prjoim/settings.py
+++ prjoim/prjoim/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-!*ju@pn8-n0@8_0)rs5da3m55)hos3fc&2k_vv+nj*lhr(tv7u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
